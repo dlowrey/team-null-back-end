fda5ee405f1c7921c0710a12a13d8da8f7745661
Dane Lowrey <dane.r.lowrey@outlook.com>
2017-03-16 00:20:59 -0500

    Added script for starting / stopping the MySQL Server

---
+++ MySQL.py
@@ -0,0 +1,17 @@
+import os
+
+# NET START / STOP MYSQLXY
+# This is for starting the MySQL Server IF it has been
+# installed as a Windows Service.
+
+def start_server():
+    """
+    Start the MySQL Server
+    """
+    os.system("NET START MYSQL57")
+
+def stop_server():
+    """
+    Stop the MySQL server
+    """
+    os.system("NET STOP MYSQL57")
