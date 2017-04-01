d70027f6ecc2904b384c285ea5066c4a6519b40a
dlowrey <dane.r.lowrey@gmail.com>
2017-04-01 15:39:53 -0500

    Refactored `initdb.py` functions and updated exception handling

--- initdb.py
+++ initdb.py
@@ -1,8 +1,6 @@
 # Initializes a MySQL Database
-
-import sys
 import mysql.connector
-from mysql.connector import errorcode
+from exceptions import InputError, MySqlError
 
 # Define constants
 DB_NAME = "HealthCareDB"
@@ -68,35 +66,28 @@ TABLES['Reports'] = (
 
 
 
-def create_database(cursor, connection):
+def create_database(cursor):
     """
     Attempt to create the DB_NAME database
     """
-    # start transaction
-    connection.start_transaction()
     try:
         create_sda_db = "CREATE DATABASE {}".format(DB_NAME)
         print("Creating database {}".format(DB_NAME))
         cursor.execute(create_sda_db)
-    except mysql.connector.Error as mysqlError:
-        print("Failed creating database: {}".format(mysqlError))
-        sys.exit()
-    # commit transactions
-    connection.commit()
+    except mysql.connector.Error as mysql_error:
+        raise MySqlError(message='Failed creating database.',
+                         args=mysql_error.args)
 
 
 def connect_to_database(connection):
     """
-    Attempt to connecect to DB_NAME database
+    Attempt to connect to DB_NAME database
     """
     try:
         connection.database = DB_NAME
-    except mysql.connector.Error as mysqlError:
-        if mysqlError.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(mysqlError)
-        sys.exit()
+    except mysql.connector.Error as mysql_error:
+        raise MySqlError(message='There was a problem connecting to the database.',
+                         args=mysql_error.args)
 
 
 def create_tables(cursor, connection):
@@ -104,53 +95,58 @@ def create_tables(cursor, connection):
     Attempt to execute each CREATE statement in TABLES
     to create tables.
     """
-    # start transaction
-    connection.start_transaction()
-    # Connect to database
     connect_to_database(connection)
     for name, query in TABLES.items():
         try:
             print("Creating table {}: ".format(name), end='')
             # Execute the CREATE xxx in TABLES
             cursor.execute(query)
-        except mysql.connector.Error as mysqlError:
-            if mysqlError.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                print("already exists in {}".format(DB_NAME))
-            elif mysqlError.errno == errorcode.ER_SYNTAX_ERROR:
-                print(mysqlError)
-            else:
-                print(mysqlError.msg)
-        else:
-            print("OK")
-
-        # commit transactions
-        connection.commit()
+        except mysql.connector.Error as mysql_error:
+            raise MySqlError(message='There was a problem creating table.',
+                             args=mysql_error.args)
 
 
-# Get username and password for desired account
-username = input("Username (root or other account): ")
-password = input("Password: ")
+def main(username='', password=''):
+    if username == "" or password == "":
+        # Get username and password for desired account
+        username = input("Username (root or other account): ")
+        password = input("Password: ")
 
-# Connect to the MySQL server with user credentials\
-try:
-    mysql_connection = mysql.connector.connect(user=username,
-                                               password=password)
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Username or Password was incorrect.")
-    else:
-        print(err)
-    sys.exit()
+    # Connect to the MySQL server with user credentials
+    # Will exit if MySQL Server is not started
+    try:
+        mysql_connection = mysql.connector.connect(user=username,
+                                                   password=password)
+    except mysql.connector.Error as err:
+        raise InputError(message='There was a problem connecting to the server.',
+                         args=err.args)
 
-# Get cursor from server connection
-mysql_cursor = mysql_connection.cursor()
-# Set autocommit to false for batch
-mysql_connection.autocommit = False
-# Create database
-create_database(mysql_cursor, mysql_connection)
-# Create tables
-create_tables(mysql_cursor, mysql_connection)
-# Close cursor
-mysql_cursor.close()
-# Close connection
-mysql_connection.close()
+    # Get cursor from server connection
+    mysql_cursor = mysql_connection.cursor()
+
+    # Set autocommit to false for batch
+    mysql_connection.autocommit = False
+
+    # Start a transaction
+    mysql_connection.start_transaction()
+
+    # Create database
+    create_database(mysql_cursor)
+
+    # Create tables
+    create_tables(mysql_cursor, mysql_connection)
+
+    # Commit transaction
+    mysql_connection.commit()
+
+    # Close cursor
+    mysql_cursor.close()
+
+    # Close connection
+    mysql_connection.close()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except (InputError, MySqlError) as db_exception:
+        print(db_exception.message)
