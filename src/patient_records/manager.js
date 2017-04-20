a85f969682c7c3cd0c5294cd6ccff89859640f1a
dlowrey <dane.r.lowrey@gmail.com>
2017-04-19 21:56:06 -0500

    Made records modifyRecord, not tested yet

--- src/patient_records/manager.js
+++ src/patient_records/manager.js
@@ -4,3 +4,26 @@ const router = express.Router();
 const bodyParser = require('body-parser'); // To parse HTML post body
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
+
+
+/**
+* modifyRecord: Update a patient record by appointment id
+*
+**/
+const modifyRecord = (req, callback) => {
+  let body = req.body; // Get body of request
+  let appointment_id = { appointment_id : req.params.uid }; // uid of appointment in URL
+  let params = { // Get field values to update to
+    weight          : body.weight,
+    height          : body.height,
+    blood_pressure  : body.blood_pressure,
+    reason          : body.reason,
+    treatment_content : body.treatment_content,
+    prescription      : body.prescription
+  };
+  // Pass uid and params as a JSONArray (order matters)
+  db.modifyRecord([params, uid], (err, response, fields) => {
+    if (err) throw err;
+    callback(params); // send back the updated appointment fields
+  });
+}
--- src/patient_records/router.js
+++ src/patient_records/router.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const manager = require('./manager.js'); // Handle request logic
 
 
-
+router.put('/:uid', (req, res) => {
+  manager.modifyRecord(req, (updatedParams) => {
+    res.status(200);
+    res.send(updatedParams);
+  });
+}
 
 // export
 module.exports = router
