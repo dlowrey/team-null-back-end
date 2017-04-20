60bd5085899bbadfc3e2007dc07769bce7c579f1
dlowrey <dane.r.lowrey@gmail.com>
2017-04-19 23:21:16 -0500

    Added appointments.id to response in modifyApp per request

--- src/appointments/manager.js
+++ src/appointments/manager.js
@@ -31,6 +31,7 @@ const modifyApp = (req, callback) => {
   let body = req.body; // Get body of request
   let uid = { id : req.params.uid }; // uid of appointment in URL
   let params = { // Get field values to update to
+    id           : req.params.uid, // needed when we send our params back
     employee_id  : body.employee_id,
     patient_id   : body.patient_id,
     date_time    : new Date(body.date_time),
