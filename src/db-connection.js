c8d190468f7839bdd82532120fe4ffe6c049ccda
dlowrey <dane.r.lowrey@gmail.com>
2017-05-02 13:22:33 -0500

    Testing design, temporary push

--- src/db-connection.js
+++ src/db-connection.js
@@ -1,13 +1,28 @@
 const mysql = require('mysql');
 
-// Establish connection
-const connection = mysql.createConnection({
-    host      :'localhost',
-    user      :'root',
-    password  :'password',
-    database  :'healthcaredb',
-    multipleStatements : true
+const dbConfig = {
+  host      :'localhost',
+  user      :'root',
+  password  :'password',
+  database  :'healthcaredb',
+  multipleStatements : true,
+  connectionLimit : 10
+};
+
+const pool = mysql.createPool(dbConfig);
+
+// Function to handle any and all database queries
+const executeSQL = (sql, params, callback) => {
+  pool.getConnection((err, cnx) => {
+    if(err) {
+      console.log(err);
+    } else {
+      let query = cnx.query(sql, params, callback);
+      cnx.release();
+      console.log('Ran query: ', query.sql);
+    }
   });
+}
 
 // Export connection
-module.exports = connection;
+module.exports = {executeSQL};
--- src/reports/db.js
+++ src/reports/db.js
@@ -1,16 +1,17 @@
-const connection = require('../db-connection.js'); // Get connection to MySQL
-
+const db = require('../db-connection.js'); // Get connection to MySQL
 
 /**
 * getAllDailyReports: get all daily reports
 * arg params: a JSONObject with {type}
 **/
 const getAllDailyReports = (params, callback) => {
-  let query = connection.query("SELECT * FROM reports WHERE ?",
-                                params, (err, response, fields) => {
-                                  callback(err, response, fields);
-                                });
-  console.log('Ran query: ' + query.sql); // Log executed sql
+  // let query = connection.query("SELECT * FROM reports WHERE ?",
+  //                               params, (err, response, fields) => {
+  //                                 callback(err, response, fields);
+  //                               });
+  // console.log('Ran query: ' + query.sql); // Log executed sql
+  let sql = "SELECT * FROM reports WHERE ?";
+  db.executeSQL(sql, params, callback);
 }
 
 /**
