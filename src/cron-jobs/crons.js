3311a013b3b73f8930494cbc74e41f7ac10f299f
dlowrey <dane.r.lowrey@gmail.com>
2017-05-06 09:43:35 -0500

    Comments

--- src/cron-jobs/crons.js
+++ src/cron-jobs/crons.js
@@ -2,17 +2,20 @@ const cron = require('node-cron');
 const db = require('./db.js');
 const mailer = require('../mailer.js');
 
-
-let markUncompleted = cron.schedule('0 21 * * *', () => {
+/**
+* markUncompleted: search for and mark uncompleted appointments at 9pm every
+*                 day, then send out penalty payment emails
+* ('0 21 * * *') = ('MIN HOUR DAYMONTH MONTH DAYWEEK')
+**/
+const markUncompleted = cron.schedule('0 21 * * *', () => {
   console.log('CRON JOB: setting uncompleted appointments.');
 
-  let completed = { completed : 2 };
-  let type = { type : 3 };
-  db.markUncompleted(completed, (response) => {
-    db.sendPenalty([completed, type],(response) => {
-      // loop through each record and send email
-      for(let i = 0; i < response.length; i++) {
-        mailer.sendPenalty(response[i]); // send the fields to the mailer
+  let completed = { completed : 2 }; // completed status for missed apps.
+  let type = { type : 3 }; // type of payment for penalties
+  db.markUncompleted(completed, (response) => { // db mark completed = 2
+    db.sendPenalty([completed, type],(response) => { // send emails
+      for(let i = 0; i < response.length; i++) { // multiple missed apps
+        mailer.sendPenalty(response[i]);
       }
     });
   });
