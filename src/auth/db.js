ecf8b9f46f820ba9ca02cba7fb563d9513b82968
dlowrey <dane.r.lowrey@gmail.com>
2017-05-07 14:11:46 -0500

    eslinted `src/auth` folder

--- src/auth/db.js
+++ src/auth/db.js
@@ -1,17 +1,14 @@
-const connection = require('../db-connection.js'); // Get connection to MySQL
+const db = require('../db-connection.js'); // Get connection to MySQL
 
 /**
 * login: Checks if there is a user with the specified id and password
 * args params: a JSON with {id,password}
 **/
 const login = (params, callback) => {
-  let query = connection.query('SELECT id, last_name, first_name, type, ' +
-                               'associated_id FROM employees WHERE ? AND ?',
-                                params, (err, response, fields) => {
-                                  callback(err, response, fields);
-                                });
-  console.log('Ran query: ' + query.sql); // Log executed sql
-}
+  const sql = 'SELECT id, last_name, first_name, type, ' +
+  'associated_id FROM employees WHERE ? AND ?';
+  db.executeSQL(sql, params, callback);
+};
 
 // Export all functions so that manager.js can find/use them in functions.
-module.exports = {login};
+module.exports = login;
--- src/auth/manager.js
+++ src/auth/manager.js
@@ -1,7 +1,8 @@
 const db = require('./db.js');
 const express = require('express');
-const router = express.Router();
 const bodyParser = require('body-parser'); // To parse HTML post body
+
+const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
@@ -10,14 +11,15 @@ router.use(bodyParser.json());
 *
 **/
 const login = (req, callback) => {
-  let body     = req.body; // get post body
-  let id       = {id : body.id};
-  let password = {password : body.password};
-  db.login([id,password], (err, response, fields) => {
+  const body = req.body; // HTTP POST body
+  const id = { id: body.id };
+  const password = { password: body.password };
+  const params = [id, password]; // Login Attributes (preserver order)
+  db.login(params, (err, response, fields) => {
     if (err) console.log(err);
-    callback(response); // send response info from query back
+    callback(response); // send JSONArray
   });
-}
+};
 
 // Export all functions so that router.js can find/use them in endpoints.
-module.exports = {login};
+module.exports = login;
