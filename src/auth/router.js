395e79586c069c6a8423d61a54c0b1e36ffa797c
NilsG-S <NilsG-S@users.noreply.github.com>
2017-05-09 02:36:23 -0500

    Fixed error handling in auth router

--- src/auth/router.js
+++ src/auth/router.js
@@ -9,13 +9,13 @@ const router = express.Router();
 * Checks if there is a user with the specified id and password
 **/
 router.post('/', (req, res) => {
-  manager.login(req,(response) => {
-    if (response) { // IF user credentials match
-        res.status(200);
-        res.send(response[0]);
+  manager.login(req, (response) => {
+    if (Object.keys(response).length !== 0) { // IF user credentials match
+      res.status(200);
+      res.send(response[0]);
     } else { // otherwise response is empty, invalid user credentials
       res.status(401);
-      res.send(response);
+      res.send();
     }
   });
 });
